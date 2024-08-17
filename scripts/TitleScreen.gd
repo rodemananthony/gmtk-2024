33d899f60922b95085237fb387a271e93ec9eabd
AveryTM <mattaver@msu.edu>
2024-08-17 15:24:38 -0400

    Changed file structure and imported title screen

---
+++ scripts/TitleScreen.gd
@@ -0,0 +1,11 @@
+extends Button
+
+
+func _on_StartButton_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/Scene 1/main.tscn")
+	pass # Replace with function body.
+
+
+func _on_Quit_pressed() -> void:
+	get_tree().quit()
+	pass # Replace with function body.
