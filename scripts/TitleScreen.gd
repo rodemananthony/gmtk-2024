29d5cacdd0dd873fd2488d2a0cc799e9961c9883
AveryTM <mattaver@msu.edu>
2024-08-17 23:52:53 -0400

    added title screen bgm, and volume slider

--- scripts/TitleScreen.gd
+++ scripts/TitleScreen.gd
@@ -10,3 +10,13 @@ func _on_StartButton_pressed() -> void:
 func _on_Quit_pressed() -> void:
 	#Quit button exits game on press
 	get_tree().quit()
+
+var master_bus = AudioServer.get_bus_index("Master")
+
+func _on_h_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(master_bus, value)
+	
+	if value == -30:
+		AudioServer.set_bus_mute(master_bus, true)
+	else:
+		AudioServer.set_bus_mute(master_bus, false)
