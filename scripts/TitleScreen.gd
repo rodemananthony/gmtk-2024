fa1e6c4803e852792c8d1acdb7eb0c6b9de1d6e5
AveryTM <mattaver@msu.edu>
2024-08-17 22:24:53 -0400

    added comments to TitleScreen.gd script

--- scripts/TitleScreen.gd
+++ scripts/TitleScreen.gd
@@ -2,10 +2,11 @@ extends Button
 
 
 func _on_StartButton_pressed() -> void:
+	#Start button redirects to the main scene on press
 	get_tree().change_scene_to_file("res://scenes/Main/main.tscn")
-	pass # Replace with function body.
+
 
 
 func _on_Quit_pressed() -> void:
+	#Quit button exits game on press
 	get_tree().quit()
-	pass # Replace with function body.
