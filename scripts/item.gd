bbee2cea0b3f4eacdbe42cb9f74a977fec95c03d
Anthony Rodeman <rodemananthony@gmail.com>
2024-08-17 22:35:28 -0400

    Update data structure definitions

--- scripts/item.gd
+++ scripts/item.gd
@@ -1,2 +1,11 @@
 class_name Item
 extends Resource
+
+## Image of the item
+@export var image: Image
+
+## Item name
+@export var name: String
+
+## Item weight in grams
+@export var weight: float # Weight in grams
--- scripts/order.gd
+++ scripts/order.gd
@@ -1,1 +1,12 @@
 class_name Order
+extends RefCounted
+
+## Item which has been ordered
+var item: Item
+
+## Reference time in milliseconds when order was created
+var order_time: int
+
+func _new(order_item: Item):
+	item = order_item
+	order_time = Time.get_ticks_msec()
