173f619dae0461c13fee73e20aec795a36bea124
AveryTM <mattaver@msu.edu>
2024-08-19 20:07:23 -0400

    debugging

--- scripts/order_ui_connector.gd
+++ scripts/order_ui_connector.gd
@@ -3,10 +3,12 @@ extends Control
 var weight_label
 var weight_needed : float = 0
 
+signal correct_order ()
+
 func _ready() -> void:
 	if self.get_tree() != null:
-		weight_label = self.get_child(3)
-		weight_needed = float(weight_label.get("text"))
+		weight_label = self.get_node("NameLabel")
+		weight_needed = float(weight_label.text)
 		#print(weight_needed)
 
 
@@ -15,10 +17,13 @@ func _on_drop_zone_package_captured(package: Package) -> void:
 		var package_weight = package.contents.weight
 		
 		if package_weight == weight_needed:
+			var test_var = %"Catalog Manager"
+			test_var.add_correct_order()
 			print("right order")
 			package.queue_free()
 			
 		elif package_weight != weight_needed:
+
 			print("wrong order")
 			package.queue_free()
 		#print(package_weight)
