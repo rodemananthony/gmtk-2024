bf0377a720af1dc48807997072ba14045843d53a
Anthony Rodeman <rodemananthony@gmail.com>
2024-08-20 01:17:44 -0400

    test out UI components/containers

--- scripts/orders_view.gd
+++ scripts/orders_view.gd
@@ -1,7 +1,5 @@
 extends Control
 
-@export var orders: Array[OrderView]
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
